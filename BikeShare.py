6dc8b6da81446fa6bc0e0448837e4f07809eec0e
Khaled Almutawa <kh.almutawa@hotmail.com>
2019-12-02 20:46:07 +0300

    add description

--- BikeShare.py
+++ BikeShare.py
@@ -75,7 +75,7 @@ def load_data(city, month, day):
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) + 1
 
-        # filter by month to create the new dataframe
+        # filter by month to create the new dataframe 
         df = df[df['month'] == month]
 
     # filter by day of week if applicable
